2c738cecd444590ca126612a1c8675cdae287416
Dominik <dominik.otto@gmail.com>
2014-04-25 11:40:40 +0200

    further implementation of reshape

--- visChi.m
+++ visChi.m
@@ -13,7 +13,7 @@ for i=1:length(chi(:,1))
     for j=1:77
         reshaped = reshape(chi(i,:),77,77);
         % probabilities
-        P1(j) = sum(reshaped(j,:))
+        P1(j) = sum(reshaped(j,:));
         P2(j) = sum(reshaped(:,j));
     end
     % rescaling for better contrast in plot
@@ -26,14 +26,8 @@ for i=1:length(chi(:,1))
         P2 = P2/(ma-mi);
     end
     % ordering the values into the 11*7 box
-    S1 = zeros(7,11);
-    S2 = zeros(7,11);
-    for z=1:7
-        for s=1:11
-            S1(z,s) = P1(Qco(z,s));
-            S2(z,s) = P2(Qco(z,s));
-        end
-    end
+    S1 = reshape(P1,11,7)';
+    S2 = reshape(P2,11,7)';
     subplot(2,1,1)
     image(S1*255);
     subplot(2,1,2);
