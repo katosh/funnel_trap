5159f54734bd5e39fab86baca3f449e2f44d1c59
Dominik <dominik.otto@gmail.com>
2014-04-25 11:24:39 +0200

    added explaining title to testCutv

--- testCurv.m
+++ testCurv.m
@@ -4,4 +4,7 @@ for i=1:20:77
     pos2 = pos2 * 255;
     figure(i)
     image(pos2)
+    p1row = ceil(i/11);
+    p1col = mod(i,11);
+    title(['Position for P2 when P1 is in row ',num2str(p1row),' and column ',num2str(p1col)])
 end
