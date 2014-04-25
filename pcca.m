2c8a0e6beeab6214d3583ad82f4aab00a7e4e2ba
Dominik <dominik.otto@gmail.com>
2014-04-25 16:48:09 +0200

    make pcca and pccaP size adaptive

--- pcca.m
+++ pcca.m
@@ -1,4 +1,5 @@
 n=4;
+len=length(P(1,:));
 [V D]=eigs(Q,n,'LA');
 [W E]=eigs(Q',n,'LA');
 D=exp(D);
@@ -16,21 +17,21 @@ end
 
 Y=X;
 ind=zeros(1,n);
-norm=zeros(77^2,1);
+norm=zeros(len,1);
 step1=true;
 
 for k=1:n
-    for j=1:77^2
+    for j=1:len
         norm(j)=Y(j,:)*Y(j,:)';
     end
     ind(k)=find(norm==max(norm),1);
 
     if step1
         step1=false;
-        Y=Y-repmat(Y(ind(k),:),77^2,1);
+        Y=Y-repmat(Y(ind(k),:),len,1);
     else
         v=Y(ind(k),:)/sqrt(Y(ind(k),:)*Y(ind(k),:)');
-        for i=1:77^2
+        for i=1:len
             Y(i,:)=Y(i,:)-(v*Y(i,:)')*v;
         end
     end
